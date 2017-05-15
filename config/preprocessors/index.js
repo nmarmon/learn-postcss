0015e8dcf2a80597587c9edcb9a86edadbcf6238
jonabasque <jonmultimedia@gmail.com>
2017-05-15 14:04:05 +0200

    Created config directory & included in gulpfile.js

---
+++ config/preprocessors/index.js
@@ -0,0 +1,14 @@
+//PREPARE CONFIG ATTRIBUTES BY ENV.
+var isProd = process.env.NODE_ENV === 'production'; //true or false
+var isDev = process.env.NODE_ENV === 'development'; //true or false
+
+var preprocessors= [];
+
+//development
+
+//production
+if(isProd){
+    
+}
+
+module.exports = preprocessors;
