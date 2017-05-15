e32677ce1899173eb7ac4baf3517a069c7875393
jonabasque <jonmultimedia@gmail.com>
2017-05-15 18:31:24 +0200

    Added 'debug-css' task & debug() wrapper for processors

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
+var debug = require('postcss-debug').createDebugger();
 //processors with custom config
 var processors = require('./config');
 
@@ -9,10 +10,14 @@ gulp.task('styles', function(){
     return gulp.src('styles.css')
         //debug inyecta código para ver los cambios en el AST y
         //devuele el array normal que necesita postcss
-        .pipe(postcss(processors))
+        .pipe(postcss(debug(processors))) //TOKNOW: meter solo para dev o para debug ??
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch:styles',function(){
   gulp.watch('**/*.css', ['styles']);
+});
+
+gulp.task('debug-css', ['styles'], function(){
+  debug.inspect();
 });
